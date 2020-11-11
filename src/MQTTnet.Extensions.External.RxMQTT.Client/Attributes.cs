85f1bec1c3de1ff85918c56edc95f44f45ffea41
mmuecke <6346606+mmuecke@users.noreply.github.com>
2020-11-11 23:14:07 +0100

    Renamed file \MQTTnet.Extensions.External.RxMQTTnet.Client.Test.csproj

--- src/MQTTnet.Extensions.External.RxMQTT.Client/Attributes.cs
+++ src/MQTTnet.Extensions.External.RxMQTT.Client/Attributes.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("MQTTnet.Extensions.External.RxMQTTnet.Client.Test")]
+[assembly: InternalsVisibleTo("MQTTnet.Extensions.External.RxMQTT.Client.Test")]
