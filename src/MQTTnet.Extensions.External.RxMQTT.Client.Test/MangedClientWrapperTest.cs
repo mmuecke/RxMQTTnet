16bcfc0b5edc629bf5dc6b217082e03635928a82
mmuecke <6346606+mmuecke@users.noreply.github.com>
2020-11-11 23:14:22 +0100

    Housekeeping Moved class MessagePayloadTransformTest to file

--- src/MQTTnet.Extensions.External.RxMQTT.Client.Test/MangedClientWrapperTest.cs
+++ src/MQTTnet.Extensions.External.RxMQTT.Client.Test/MangedClientWrapperTest.cs
@@ -15,21 +15,6 @@ using Xunit;
 
 namespace MQTTnet.Extensions.External.RxMQTT.Client.Test
 {
-    // the function is testet via the public extension methods
-    public class MessagePayloadTransformTest
-    {
-        [Fact]
-        public void CTOR_ArgumentNullExcepoin()
-        {
-            Assert.Throws<ArgumentNullException>(() => new MessagePayloadTransform<byte[]>(null, p => p, false));
-        }
-
-        [Fact]
-        public void CTOR_ArgumentNullExcepoin_GetPayloadFunc()
-        {
-            Assert.Throws<ArgumentNullException>(() => new MessagePayloadTransform<byte[]>(Observable.Never<MqttApplicationMessage>(), null, false));
-        }
-    }
 
     public class MangedClientWrapperTest
     {
---
+++ src/MQTTnet.Extensions.External.RxMQTT.Client.Test/MessagePayloadTransformTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using Xunit;
+
+namespace MQTTnet.Extensions.External.RxMQTT.Client.Test
+{
+    // the function is testet via the public extension methods
+    public class MessagePayloadTransformTest
+    {
+        [Fact]
+        public void CTOR_ArgumentNullExcepoin()
+        {
+            Assert.Throws<ArgumentNullException>(() => new MessagePayloadTransform<byte[]>(null, p => p, false));
+        }
+
+        [Fact]
+        public void CTOR_ArgumentNullExcepoin_GetPayloadFunc()
+        {
+            Assert.Throws<ArgumentNullException>(() => new MessagePayloadTransform<byte[]>(Observable.Never<MqttApplicationMessage>(), null, false));
+        }
+    }
+}
