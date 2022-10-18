b27b0e38082bab165d9989823de312d280e94e80
mmuecke <6346606+mmuecke@users.noreply.github.com>
2022-10-18 22:12:43 +0200

    Fixed unit tests for connect / disconnect event

--- src/MQTTnet.Extensions.External.RxMQTT.Client.Test/MangedClientWrapperTest.cs
+++ src/MQTTnet.Extensions.External.RxMQTT.Client.Test/MangedClientWrapperTest.cs
@@ -9,6 +9,7 @@ using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace MQTTnet.Extensions.External.RxMQTT.Client.Test
@@ -144,11 +145,12 @@ namespace MQTTnet.Extensions.External.RxMQTT.Client.Test
             var rxMqttClinet = mock.Create<RxMqttClient>();
 
             var testScheduler = new TestScheduler();
-
+            Func<MqttClientConnectedEventArgs, Task> d = null;
             testScheduler.Schedule(TimeSpan.FromTicks(2), () =>
-                mock.Mock<IManagedMqttClient>().Raise(x => x.ConnectedAsync += null, (object)EventArgs.Empty));
+                mock.Mock<IManagedMqttClient>().Raise(x => x.ConnectedAsync += null, (Func<MqttClientConnectedEventArgs, Task>)null));
             testScheduler.Schedule(TimeSpan.FromTicks(3), () =>
-                mock.Mock<IManagedMqttClient>().Raise(x => x.DisconnectedAsync += null, (object)EventArgs.Empty));
+                mock.Mock<IManagedMqttClient>().Raise(x => x.DisconnectedAsync += null, (Func<MqttClientDisconnectedEventArgs, Task>)null));
+           
             // act
             var testObserver = testScheduler.Start(() => rxMqttClinet.Connected, 0, 1, 4);
 
